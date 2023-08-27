1c9e23886d813c49d99aa1775cd21dc671259b7c
Nakkex <143256404+Nakkex@users.noreply.github.com>
2023-08-27 15:08:07 -0300

    Update raw.js

--- events/raw.js
+++ events/raw.js
@@ -1,4 +1,7 @@
 module.exports = (client, data) => {
-  //What is data? Discord Gateway Data, Please check discord api docs
+  // ¿Qué es 'data'? Datos de la Puerta de Enlace de Discord. Por favor, consulta la documentación de la API de Discord.
+  // El parámetro 'data' contiene información sobre el evento de cambio de estado de voz en Discord.
+
+  // Actualiza el estado de voz en el cliente de música
   client.Manager.updateVoiceState(data);
 };
