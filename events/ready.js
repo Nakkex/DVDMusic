78972061f58a3175d95daf932324132de57fa864
Nakkex <143256404+Nakkex@users.noreply.github.com>
2023-08-27 15:10:12 -0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,13 +1,22 @@
 module.exports = async (client) => {
-  (client.Ready = true),
-    client.user.setPresence({
-      status: client.botconfig.Presence.status, // You can show online, idle, and dnd
-      activity: {
-        name: client.botconfig.Presence.name,
-        type: client.botconfig.Presence.type,
-      },
-    });
+  // Establece la propiedad 'Ready' en true en el cliente
+  client.Ready = true;
+
+  // Configura la presencia del bot en Discord
+  client.user.setPresence({
+    status: client.botconfig.Presence.status, // Estado del bot: online, idle, dnd (no molestar)
+    activity: {
+      name: client.botconfig.Presence.name, // Nombre de la actividad (por ejemplo, "Listening to Music")
+      type: client.botconfig.Presence.type, // Tipo de actividad: PLAYING, LISTENING, WATCHING, STREAMING
+    },
+  });
+
+  // Inicializa el cliente de música (Manager) utilizando el ID del bot
   client.Manager.init(client.user.id);
-  client.log("Successfully Logged in as " + client.user.tag); // You can change the text if you want, but DO NOT REMOVE "client.user.tag"
+
+  // Registra comandos de barra en Discord
   client.RegisterSlashCommands();
+
+  // Muestra un mensaje en la consola indicando que el bot ha iniciado sesión exitosamente
+  client.log("Successfully Logged in as " + client.user.tag);
 };
