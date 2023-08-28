7af0656305ad3ac5876adcd6bddb6635f623c8d8
Nakkex <143256404+Nakkex@users.noreply.github.com>
2023-08-28 03:02:44 -0300

    Update seek.js

--- commands/seek.js
+++ commands/seek.js
@@ -3,8 +3,8 @@ const { TrackUtils } = require("erela.js");
 
 module.exports = {
   name: "seek",
-  description: "Seek to a position in the song",
-  usage: "<time s/m/h>",
+  description: "Avanzar a una posición en la canción",
+  usage: "<tiempo s/m/h>",
   permissions: {
     channel: ["VIEW_CHANNEL", "SEND_MESSAGES", "EMBED_LINKS"],
     member: [],
@@ -22,12 +22,12 @@ module.exports = {
     if (!player)
       return client.sendTime(
         message.channel,
-        "❌ | **Nothing is playing right now...**"
+        "❌ | **No hay nada reproduciéndose en este momento...**"
       );
     if (!message.member.voice.channel)
       return client.sendTime(
         message.channel,
-        "❌ | **You must be in a voice channel to use this command!**"
+        "❌ | **Debes estar en un canal de voz para usar este comando.**"
       );
     if (
       message.guild.me.voice.channel &&
@@ -35,54 +35,20 @@ module.exports = {
     )
       return client.sendTime(
         message.channel,
-        "❌ | **You must be in the same voice channel as me to use this command!**"
+        "❌ | **Debes estar en el mismo canal de voz que yo para usar este comando.**"
       );
     if (!player.queue.current.isSeekable)
       return client.sendTime(
         message.channel,
-        "❌ | **I'm not able to seek this song!**"
+        "❌ | **No puedo avanzar en esta canción.**"
       );
     let SeekTo = client.ParseHumanTime(args.join(" "));
     if (!SeekTo)
       return client.sendTime(
         message.channel,
-        `**Usage - **\`${GuildDB.prefix}seek <number s/m/h>\` \n**Example - **\`${GuildDB.prefix}seek 2m 10s\``
+        `**Uso - **\`${GuildDB.prefix}seek <número s/m/h>\` \n**Ejemplo - **\`${GuildDB.prefix}seek 2m 10s\``
       );
     player.seek(SeekTo * 1000);
     message.react("✅");
   },
-  /*
-    SlashCommand: {
-        options: [
-            {
-                name: "position",
-                description: "Enter a timestamp you want to seek to. Example - 2m 10s",
-                value: "position",
-                type: 3,
-                required: true,
-                //**
-                *
-                * @param {import("../structures/DiscordMusicBot")} client
-                * @param {import("discord.js").Message} message
-                * @param {string[]} args
-                * @param {*} param3
-                *
-                run: async (client, interaction, args, { GuildDB }) => {
-                    const guild = client.guilds.cache.get(interaction.guild_id);
-                    const member = guild.members.cache.get(interaction.member.user.id);
-                    let player = await client.Manager.get(interaction.guild_id);
-                    
-                    if (!member.voice.channel) return client.sendTime(interaction, "❌ | **You must be in a voice channel to use this command.**");
-                    if (guild.me.voice.channel && !guild.me.voice.channel.equals(member.voice.channel)) return client.sendTime(interaction, "❌ | **You must be in the same voice channel as me to use this command!**");
-                    if (!player) return client.sendTime(interaction, "❌ | **Nothing is playing right now...**");
-                    if (!player.queue.current.isSeekable) return client.sendTime(interaction, "❌ | **I'm not able to seek this song!**");
-                    let SeekTo = client.ParseHumanTime(interaction.data.options[0].value);
-                    if (!SeekTo) return client.sendTime(interaction, `**Usage - **\`${GuildDB.prefix}seek <number s/m/h>\` \n**Example -** \`${GuildDB.prefix}seek 2m 10s\``);
-                    player.seek(SeekTo * 1000);
-                    client.sendTime(interaction, "✅ | **Successfully moved the song to **", `\`${Seekto}\``);
-                },
-            },
-        ],
-    },
-*/
 };
