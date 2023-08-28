b632e8ba610be331dfeb3589c363610bd796e6d8
Nakkex <143256404+Nakkex@users.noreply.github.com>
2023-08-28 03:05:12 -0300

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -4,15 +4,16 @@ module.exports = {
   DefaultPrefix: process.env.Prefix || ">",
   Port: 80, //Which port website gonna be hosted
   SupportServer: "https://discord.io/sciencegear", // Support Server Link
-  Token: process.env.Token || "", // Discord Bot Token
-  ClientID: process.env.Discord_ClientID || "", 
-  ClientSecret: process.env.Discord_ClientSecret || "", 
+  Token: process.env.Token || "MTE0NTU4MjQ3ODc3NDUwNTQ3Mw.GcCSrl.bwQ8KDLPoEU7LWrHtLXekixsxhFmvzQqmz4RAs
+", // Discord Bot Token
+  ClientID: process.env.Discord_ClientID || "1145582478774505473", 
+  ClientSecret: process.env.Discord_ClientSecret || "DMAcfnE6XLgL5GJC92X2ovlaxgja9D99", 
   Scopes: ["identify", "guilds", "applications.commands"],
   ServerDeafen: true, 
   DefaultVolume: 100,
   CallbackURL: "/api/callback", 
   "24/7": false,
-  CookieSecret: "Pikachu is cute",
+  CookieSecret: "Coscu es GOD",
   IconURL:
     "https://media.tenor.com/sce9SDRey4EAAAAi/disc.gif",
   EmbedColor: "RANDOM", 
@@ -27,17 +28,17 @@ module.exports = {
 
   Lavalink: {
     id: "Main",
-    host: "lavalink2.devamop.in", 
-    port: 8830, 
-    pass: "DevamOP", 
-    secure: false, 
+    host: "ssl.horizxon.studio", 
+    port: 443, 
+    pass: "horizxon.studio", 
+    secure: true, 
     retryAmount: 200, 
     retryDelay: 40, 
   },
   
   Spotify: {
-    ClientID: process.env.Spotify_ClientID || "", 
-    ClientSecret: process.env.Spotify_ClientSecret || "", 
+    ClientID: process.env.Spotify_ClientID || "cf29400665ce457bbb4a39ac818893d6", 
+    ClientSecret: process.env.Spotify_ClientSecret || "d085587513aa43faa3ebfbdfac1a273a", 
   },
 
 
