f99910d18506b5137cd4ab5882446154beee596d
Nakkex <143256404+Nakkex@users.noreply.github.com>
2023-08-28 03:13:08 -0300

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -4,9 +4,9 @@ module.exports = {
   DefaultPrefix: process.env.Prefix || ">",
   Port: 80, //Which port website gonna be hosted
   SupportServer: "https://discord.io/sciencegear", // Support Server Link
-  Token: process.env.Token || "", // Discord Bot Token
+  Token: process.env.Token || "MTE0NTU4MjQ3ODc3NDUwNTQ3Mw.G6kto9.1H9tkc1-cylsH8Np6TpmaOiwVZkPGXWniLYg7Y", // Discord Bot Token
   ClientID: process.env.Discord_ClientID || "", 
-  ClientSecret: process.env.Discord_ClientSecret || "", 
+  ClientSecret: process.env.Discord_ClientSecret || "X12g9l9d5TuTbQVJ6Db4yJ6UBh-CQ_Hp", 
   Scopes: ["identify", "guilds", "applications.commands"],
   ServerDeafen: true, 
   DefaultVolume: 100,
@@ -27,9 +27,9 @@ module.exports = {
 
   Lavalink: {
     id: "Main",
-    host: "", 
+    host: "ssl.horizxon.studio", 
     port: 443, 
-    pass: "", 
+    pass: "horizxon.studio", 
     secure: true, 
     retryAmount: 200, 
     retryDelay: 40, 
